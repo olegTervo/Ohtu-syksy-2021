8e883ee23610d52c193980c8b74fe833012feab9
olegTervo <oleg.tervo-ridor@helsinki.fi>
2021-11-18 21:33:27 +0200

    v3 t1-2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,31 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+#    print("JSON-muotoinen vastaus:")
+#    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['goals'],
+            player_dict['assists']
+        )
+
+        if player_dict['nationality'] == 'FIN':
+            players.append(player)
+    
+    players = sorted(players, key=lambda p: p.points, reverse=True)
+    print("Oliot:")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
+
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, goals, assists):
         self.name = name
-    
+        self.goals = goals
+        self.assists = assists
+
+    @property
+    def points(self):
+        return self.goals + self.assists
+
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.goals} + {self.assists} = {self.points}"
+

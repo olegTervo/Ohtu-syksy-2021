36bf7ff199b31cf7e3715358b22009f4f7f79b2a
olegTervo <oleg.tervo-ridor@helsinki.fi>
2021-12-06 23:54:33 +0200

    v5 t4-5

---
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -0,0 +1,21 @@
+class Sovelluslogiikka:
+    def __init__(self, tulos=0):
+        self.tulos = tulos
+
+    def miinus(self, arvo):
+        self.tulos = self.tulos - arvo
+
+    def plus(self, arvo):
+        self.tulos = self.tulos + arvo
+
+    def nollaa(self):
+        self.tulos = 0
+
+    def aseta_arvo(self, arvo):
+        self.tulos = arvo
+
+    def tallenna(self, arvo):
+        self.historia = arvo
+
+    def get_last_saved(self):
+        return self.historia
